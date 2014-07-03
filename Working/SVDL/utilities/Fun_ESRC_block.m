bccd6f1e89e3780b79ff7a9d0f7524de427d4bc3
bmetge <baptiste.metge@insa-lyon.fr>
2014-07-03 17:55:42 +0800

    uses with Fun_ESRC_Block instead of Fun_ESRC_gaps_Block when it makes more sense

---
+++ Working/SVDL/utilities/Fun_ESRC_block.m
@@ -0,0 +1,36 @@
+function correct_rate = Fun_ESRC_block(Train_M,train_label,Test_M,test_label,lambda)
+% lambda is the parameter of the coefficient's regularization, e.g., lambda = 1e-3
+% trainlabel is a set of numbers from 1 to nCls;
+addpath('./SVDL/L1Solvers');
+addpath ./SVDL/utilities;
+% addpath('l1_ls_matlab');
+
+nDim     =  size(Train_M,1);
+nNum     =  length(train_label);
+label    =  unique(train_label);
+nCls     =  length(label);
+Train_M  =  Train_M./( repmat(sqrt(sum(Train_M.*Train_M)), [nDim,1]) );
+Test_M   =  Test_M./( repmat(sqrt(sum(Test_M.*Test_M)), [nDim,1]) );
+
+cor_gap  = [];
+wro_gap  = [];
+
+for ti = 1:size(Test_M,2)
+    test = rem((ti-8),20);
+    if test ~= 0
+        disp(num2str(ti));
+        y  = Test_M(:,ti);
+        [x, nIter] = SolveDALM(Train_M,y, 'lambda',lambda,'tolerance',1e-3); % SolveDALM correspond à Dual Augmented Lagrange Multiplier 
+        y_add = Train_M(:,nNum+1:end)*x(nNum+1:end,1);
+        for ci = 1:nCls
+            class = label(ci);
+            cdat = Train_M(:,train_label==class);
+            er   = y - cdat*x(train_label==class) - y_add;
+            gap(ci) = er(:)'*er(:); % correspond à ei page5 du paper je pense ou alors un vecteur qui contint tout les ei
+        end
+        index = find(gap == min(gap)); % ne suffirait-il pas de sommmer les gaps de chacun des features et ensuite faire le min sur la somme ?
+        ID(ti) = label(index(1));
+    end
+end
+
+correct_rate = sum(ID==test_label)/length(test_label);
